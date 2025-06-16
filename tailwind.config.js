6bd82f18c52f8b5ba4d521647671eab034447d25
Slezy18 <tomaspatrikdavid@seznam.cz>
2025-06-17 01:46:30 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e0f0ff",
+        accent: "#4da6ff"
+      }
+    }
+  },
+  plugins: [],
+}
